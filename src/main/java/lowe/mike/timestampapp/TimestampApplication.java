2cef7d81d952e4c1f3f61067979879ccfe130ef7
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-04-29 12:36:50 +0100

    Initial setup

---
+++ src/main/java/lowe/mike/timestampapp/TimestampApplication.java
@@ -0,0 +1,13 @@
+package lowe.mike.timestampapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimestampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimestampApplication.class, args);
+    }
+
+}
